0cea56f21817f9ec81746c3ce0940c769edb4bac
Yerikho William Tasilima <yerikhowilliamt@gmail.com>
2024-11-10 00:11:47 +0700

    add: cors middleware

--- src/main.ts
+++ src/main.ts
@@ -6,6 +6,8 @@ dotenv.config();
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  app.enableCors();
+
   const port = process.env.PORT
   await app.listen(port);
 }
